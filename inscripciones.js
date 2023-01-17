66fdf83bd7d5cb1ae62d43b5a72d2a9720e88c7c
Nicolas Ferreira <ferreira.nicolas.et21.21@gmail.com>
2023-01-17 19:45:48 -0300

    agregada pagina de inscripciones (falta arreglar)

---
+++ inscripciones.js
@@ -0,0 +1,21 @@
+
+
+var form1 = document.getElementById("form1");
+var form2 = document.getElementById("form2");
+
+var next = document.getElementById("continuar");
+var back = document.getElementById("atras");
+var confirm = document.getElementById("confirmar")
+
+window.onload = function(){
+    next.onclick = function(){
+        form1.style.left = "700px";
+        form2.style.left = "20px";
+        
+    }
+    back.onclick = function(){
+        form1.style.left = "20px";
+        form2.stlye.left = "700px";
+    }
+    
+}
